d07224ec433521d004704195255f5b8f820f1533
ShkoliarenkoTech <shkoliarenkod@gmail.com>
2022-10-08 21:16:55 +0200

    .
    .

--- Assets/Input System/TestInputSystemJump.cs
+++ Assets/Input System/TestInputSystemJump.cs
@@ -2,20 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.InputSystem;
+
 public class TestInputSystemJump : MonoBehaviour
 {
     private Rigidbody rigidbodyObject;
+    private PlayerInput playerInput;
 
     private void Awake()
     {
         rigidbodyObject = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+
+        PlayerInputAction playerInputAction = new PlayerInputAction();
+        playerInputAction.Player.Enable();
+        playerInputAction.Player.Jump.performed += Jump;
+
+        playerInputAction.Player.Movement.performed += Movement_performed;
     }
 
 
+    //movement performed
 
-    public void Jump()
+
+    private void Movement_performed(InputAction.CallbackContext context)
     {
-       Debug.Log("Jump!");
-       rigidbodyObject.AddForce(Vector3.up * 0.5f,ForceMode.Impulse);
+        Debug.Log(context);
+        Vector2 inputVector2 = context.ReadValue<Vector2>();
+        float speed = 10f;
+        rigidbodyObject.AddForce(new Vector3(inputVector2.x, 0, inputVector2.y)* speed, ForceMode.Force); 
+    }
+    public void Jump(InputAction.CallbackContext context)
+    {
+
+            Debug.Log("Jump! " + context.phase);
+            rigidbodyObject.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+
     }
 }
